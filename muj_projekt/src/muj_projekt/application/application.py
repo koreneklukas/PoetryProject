21e28000e90181a53bfc4a6f1ba4ac033cf5c9b1
koreneklukas <70533829+koreneklukas@users.noreply.github.com>
2025-06-06 21:47:46 +0200

    test create poetry

---
+++ muj_projekt/src/muj_projekt/application/application.py
@@ -0,0 +1,9 @@
+
+class Application:
+	def hello(self):
+		print("Hello world")
+
+	def cycle(self):
+
+		for i in range(6):
+			print(i)
--- muj_projekt/src/muj_projekt/main.py
+++ muj_projekt/src/muj_projekt/main.py
@@ -1,1 +1,11 @@
 import requests
+from application.application import Application
+
+
+def main():
+	app = Application()
+	app.hello()
+	app.cycle()
+
+if __name__ == "__main__":
+	main()
