104f3069ba5601e6c429d65f8e41138543cb24c7
koreneklukas <70533829+koreneklukas@users.noreply.github.com>
2025-06-08 22:42:50 +0200

    Testing of unittests

--- tests/test_application.py
+++ tests/test_application.py
@@ -7,7 +7,7 @@ app = Application()
 class TestApplication:
 
 	def test_math_operation_summation(self):
-		assert app.math_operation_summation(5,2) == 7
+		assert app.math_operation_summation(5,2) == 6
 
 	def test_math_operation_division(self):
 		assert app.math_operation_division(10,2) == 5
